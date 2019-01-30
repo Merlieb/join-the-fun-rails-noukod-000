4fab49df165c8049bd8602fc2aedc60df5d87deb
Merline Belizaire <merline@noukod.com>
2019-01-30 17:11:48 +0000

    Automatically backed up by Learn

--- db/migrate/20190130171120_create_add_foreign_keys_to_ride.rb
+++ db/migrate/20190130171120_create_add_foreign_keys_to_ride.rb
@@ -1,6 +1,7 @@
 class CreateAddForeignKeysToRide < ActiveRecord::Migration
   def change
-    create_table :add_foreign_keys_to_rides do |t|
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
     end
   end
 end
